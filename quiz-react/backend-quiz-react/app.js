df378a4a2d17d2da3ec3918679274644ea3cb926
JariMiers04 <56120363+JariMiers04@users.noreply.github.com>
2021-10-11 13:44:59 +0200

    2 ways of connecting with database prefer express

--- quiz-react/backend-quiz-react/app.js
+++ quiz-react/backend-quiz-react/app.js
@@ -1,29 +1,128 @@
+'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const port = process.env.PORT || 3000;
+
 const teacherQuestions = require("./repos/teacherQuestions");
-const data = require("./questionExample.json");
+
+const app = express();
+const router = express.Router();
 
 
 const { MongoClient } = require('mongodb');
 const url = "mongodb+srv://admin:JariMiers04@quiz.etjsy.mongodb.net/quiz?retryWrites=true&w=majority";
+const dbName = 'questionsTeacher';
+const dbClient = new MongoClient(url);
+
+app.use(cors());
+
+dbClient.connect(()=>{
+  console.log("Connected to db");
+});
 
 
-async function main(){
-  const client = new MongoClient(url);
-  await client.connect();
+app.use(router);
 
-  const results = await teacherQuestions.loadData(data);
-  console.log(results.insertedCount, results.ops)
-  const admin = client.db('quiz').admin();
-  // console.log(await admin.serverStatus());
-  console.log(await admin.listDatabases());
-}
+// async function run() {
+//   try {
+//     await dbClient.connect();
+//     console.log("Connected correctly to server");
+//     const db = dbClient.db(dbName);
 
-main();
+//     // Use the collection
+//     const col = db.collection("questions");
 
-// const port = process.env.PORT || 3000;
+//     // Construct a document                                                                                                                                                              
+//     let newQuestion = {
+//       "question" : "What is 6*2",
+//       "correctAnswer": "12",
+//       "wrongAnsers": [
+//           "1",
+//           "18",
+//           "2"
+//       ]
+//   }
+
+//     // Insert a single document, wait for promise so we can read it back
+//     const p = await col.insertOne(newQuestion);
+//     // Find one document
+//     const myDoc = await col.findOne();
+//     // Print to the console
+//     console.log(myDoc);
+
+//   } catch (err) {
+//     console.log(err.stack);
+//   } finally {
+//     await dbClient.close();
+//   }
+// }
+
+// run().catch(console.dir);
+
+
+router.route('/').get(async(req, res)=>{
+  console.log("this is the index page");
+  res.send("Hello, welcome to my index page");
+});
+
+router.route('/teacher').post(async(req,res)=>{
+  const db = dbClient.db(dbName);
+
+  const collection = db.collection("questions");
+
+  const{question, correctAnswer, wrongAnswer: []} = req.body;
+  console.log(req.body);
+
+  let newQuestion = {
+    question,
+    correctAnswer,
+    wrongAnswer
+  }
+
+  console.log(newQuestion);
+
+  collection.insertOne(newQuestion);
+  res.send(newQuestion);
+})
+
+app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`))
+
+// async function main(){
+//   const client = new MongoClient(url);
+//   await client.connect();
+//   try {   
+//     const results = await teacherQuestions.loadData();
+
+//     const getData = await teacherQuestions.get();
+
+//     const newItem = {
+//       "question" : "How many students are there in WEB3?",
+//       "correctAnswer": "23",
+//       "wrongAnsers": [
+//           "19",
+//           "4",
+//           "22"
+//       ]
+//   };
+
+//   const addItem = await teacherQuestions.add(newItem);
+
+//   } catch (error) {
+//     console.log(error);
+//   } finally{
+//     const admin = client.db('quiz').admin();
+
+//     await client.db('questionsTeacher').dropDatabase();
+//     console.log(await admin.listDatabases());
+  
+//     client.close();
+//   }
+// }
+
+// main();
 
 // const app = express();
 // const router = express.Router();
--- quiz-react/backend-quiz-react/repos/teacherQuestions.js
+++ quiz-react/backend-quiz-react/repos/teacherQuestions.js
@@ -2,6 +2,43 @@ const {MongoClient} = require('mongodb');
 
 function teacherQuestions(){
     const url = "mongodb+srv://admin:JariMiers04@quiz.etjsy.mongodb.net/quiz?retryWrites=true&w=majority";
+    
+    function get(){
+        return new Promise (async (resolve, reject)=>{
+            const client = new MongoClient(url);
+            try{
+                await client.connect();
+                const db = client.db('questionsTeacher');
+
+
+                const items = db.collection('questions').find();
+
+                resolve(await items.toArray());
+                client.close();
+            }catch(err){
+                reject(err);
+            }
+        })
+    }
+
+    function add(item){
+        return new Promise(async(resolve,reject)=>{
+            const client = new MongoClient(url);
+
+            try {
+                await client.connect();
+                const db = client.db('questionTeacher');
+                const addedItem = await db.collection('questions').insertOne(item);
+
+                console.log(addedItem);
+
+                resolve(addedItem)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+    
     function loadData(){
         return new Promise(async(resolve,reject)=>{
             const client = new MongoClient(url);
@@ -18,9 +55,19 @@ function teacherQuestions(){
                         "3",
                         "5"
                     ]
+                },
+                {
+                    "question" : "What is 3*2",
+                    "correctAnswer": "6",
+                    "wrongAnsers": [
+                        "5",
+                        "9",
+                        "8"
+                    ]
                 }
             );
               resolve(results);
+              client.close();
             }
             catch(err){
                 reject(err);
@@ -28,7 +75,7 @@ function teacherQuestions(){
         })
     }
 
-    return{loadData}
+    return{loadData, get, add}
 }
 
 module.exports = teacherQuestions();
